f3d50962d8cbb11450dcbb297c78e6b27e44b3f1
tako <takosama.312@gmail.com>
2018-03-04 15:52:34 +0900

    fomat

--- FastAtanV4/Program.cs
+++ FastAtanV4/Program.cs
@@ -2,53 +2,52 @@
 using System.Diagnostics;
 
 class Program
+{
+    static void Main(string[] args)
     {
 
-         static void Main(string[] args)
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < 10000000; i++)
         {
+            i++;
+            i--;
+        }
+        double ans = 0;
+        for (int c = 0; c < 10; c++)
+        {
+            sw.Start();
 
-            Stopwatch sw = new Stopwatch();
             for (int i = 0; i < 10000000; i++)
             {
-                i++;
-                i--;
+                ans = Math.Atan(i * 0.00001);
+                //      ans = Atan.Compute(i*0.00001);
+
+                //      ans = Math.Cos(0.01 * i);
+                //   ans = Cos.Compute(0.01f * i);
+
+
             }
-            double ans = 0;
-            for (int c = 0; c < 10; c++)
-            {
-                sw.Start();
 
-                for (int i = 0; i < 10000000; i++)
-                {
-                    ans = Math.Atan(i * 0.00001);
-                    //      ans = Atan.Compute(i*0.00001);
-
-                    //      ans = Math.Cos(0.01 * i);
-                    //   ans = Cos.Compute(0.01f * i);
-
-
-                }
-
-                Console.WriteLine(sw.ElapsedMilliseconds);
-                Console.WriteLine(ans);
-                Console.WriteLine();
-                sw.Reset();
-            }
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.WriteLine(ans);
+            Console.WriteLine();
+            sw.Reset();
         }
     }
+}
 
-    static class Atan
+static class Atan
+{
+
+    public static double Compute(double x)
     {
-
-        public static double Compute(double x)
-        {
-            if (x > -2.5 && x < 2.5)
-                return Math.Atan(x);
-            if (x < 0)
-                return -1.57079633 - 1.0 / x;
-            else
-                return 1.57079633 - 1.0 / x;
-        }
+        if (x > -2.5 && x < 2.5)
+            return Math.Atan(x);
+        if (x < 0)
+            return -1.57079633 - 1.0 / x;
+        else
+            return 1.57079633 - 1.0 / x;
     }
+}
 
 
